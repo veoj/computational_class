a2a91b7cc5f7dbf0332493fa6263d252649279e3
veoj <j.sun@tcu.edu>
2017-05-03 16:28:43 -0500

    project

---
+++ project/JingSun_project_shield_first.py
@@ -0,0 +1,305 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import random as ran
+import numpy as np
+import pylab as py
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import math as math
+
+
+# In[2]:
+
+h=1
+tmax=100
+tpoints=np.arange(0,tmax+1,h)
+
+sample=10000
+s_points=np.arange(0,sample+1,h)
+
+
+# In[3]:
+
+first=1
+p_same=2/3
+p_change=1/3
+
+
+# In[4]:
+
+ele=first
+pos=first
+position=[]
+
+
+# In[5]:
+
+up=2
+down=-2
+right=1
+left=-1
+
+
+# In[6]:
+
+# loop for capture possibility
+for i in s_points:
+    position.append(pos)
+    ele = 1
+    # 2nd move        
+    if ran.random() > p_change:
+        ele += right
+        test = right
+    else:
+        if ran.random() > 1/2:
+            ele += 0*up
+            test = up
+        else:
+            ele += 0*down
+            test = down
+    # 3rd move  
+    if test == right:
+        if ran.random() > p_change:
+            ele += right
+            test = right
+        else:
+            if ran.random() > 1/2:
+                ele += 0*up
+                test = up
+            else:
+                ele += 0*down
+                test = down
+    else:
+        if test == up:
+            if ran.random() > p_change:
+                ele += 0*up
+                test = up
+            else:
+                if ran.random() > 1/2:
+                    ele += left
+                    test = left
+                else:
+                    ele += right
+                    test = right
+        else:
+            if test == down:
+                if ran.random() > p_change:
+                    ele += 0*down
+                    test = down
+                else:
+                    if ran.random() > 1/2:
+                        ele += left
+                        test = left
+                    else:
+                        ele += right
+                        test = right
+        # 4th and later move
+        for t in range(3,tmax+1):
+            if test == right:
+                if ran.random() > p_change:
+                    ele += right
+                    test = right
+                else:
+                    if ran.random() > 1/2:
+                        ele += 0*up
+                        test = up
+                    else:
+                        ele += 0*down
+                        test = down
+            else:
+                if test == up:
+                    if ran.random() > p_change:
+                        ele += 0*up
+                        test = up
+                    else:
+                        if ran.random() > 1/2:
+                            ele += left
+                            test = left
+                        else:
+                            ele += right
+                            test = right
+                else:
+                    if test == down:
+                        if ran.random() > p_change:
+                            ele += 0*down
+                            test = down
+                        else:
+                            if ran.random() > 1/2:
+                                ele += left
+                                test = left
+                            else:
+                                ele += right
+                                test = right
+                    else:
+                        if test == left:
+                            if ran.random() > p_change:
+                                ele += left
+                                test = left
+                            else:
+                                if ran.random() > 1/2:
+                                    ele += 0*up
+                                    test = up
+                                else:
+                                    ele += 0*down
+                                    test = down
+    pos = ele
+
+
+# In[7]:
+
+print(position)
+
+
+# In[8]:
+
+position=position[1:sample+1]
+print(len(position))
+
+
+# In[9]:
+
+reactor=[]
+capture=[]
+through=[]
+
+num_r=0
+num_c=0
+num_t=0
+
+size=np.arange(0,tmax+1,h)
+print(len(size))
+
+
+# In[10]:
+
+for j in range(tmax+1):
+    reactor.append(num_r)
+    capture.append(num_c)
+    through.append(num_t)
+    x = size[j]
+    nt=0
+    nc=0
+    nr=0
+    for i in range(sample):
+        if position[i] > x:
+            nt += 1
+        elif position[i] > 0:
+            nc += 1
+        else:
+            nr += 1
+    if nr == 0:
+        num_r = 0.001
+    else:
+        num_r = nr
+    if nc == 0:
+        num_c = 0.001
+    else:
+        num_c = nc
+    if nt == 0:
+        num_t = 0.001
+    else:
+        num_t = nt
+
+
+# In[11]:
+
+reactor_n=reactor[1:tmax+1]
+capture_n=capture[1:tmax+1]
+through_n=through[1:tmax+1]
+
+
+# In[12]:
+
+print(reactor_n)
+print(len(reactor_n))
+
+
+# In[13]:
+
+print(capture_n)
+print(len(capture_n))
+
+
+# In[14]:
+
+print(through_n)
+print(len(through_n))
+
+
+# In[15]:
+
+size=size[1:tmax+1]
+print(size)
+print(len(size))
+
+
+# In[16]:
+
+size_array=np.array(size)
+through_array=np.array(through_n)
+capture_array=np.array(capture_n)
+reactor_array=np.array(reactor_n)
+plt.plot(size_array,through_array,'-',label='through',color='red',linewidth=3.0)
+plt.plot(size_array,capture_array,'-',label='capture',color='blue',linewidth=3.0)
+plt.plot(size_array,reactor_array,'-',label='reactor',color='black',linewidth=3.0)
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel('Size of the shield')
+plt.ylabel('Number')
+plt.show()
+
+
+
+
+
+# In[17]:
+
+P_through=through_array/sample
+P_capture=capture_array/sample
+P_reactor=reactor_array/sample
+
+
+# In[18]:
+
+m_through,b_through=np.polyfit(size,np.log(P_through),1)
+print(m_through,b_through)
+
+
+fit_t=m_through*size+b_through
+uncertainty_t=(1/tmax)*np.sum((fit_t-np.log(P_through))**2)#Standard Deviation
+print(math.sqrt(uncertainty_t))
+
+
+# In[19]:
+
+m_capture,b_capture=np.polyfit(size,np.log(P_capture),1)
+print(m_capture,b_capture)
+
+
+fit_c=m_capture*size+b_capture
+uncertainty_c=(1/tmax)*np.sum((fit_t-np.log(P_capture))**2)#Standard Deviation
+print(math.sqrt(uncertainty_c))
+
+
+# In[20]:
+
+m_reactor,b_reactor=np.polyfit(size,np.log(P_reactor),1)
+print(m_reactor,b_reactor)
+
+
+fit_t=m_reactor*size+b_reactor
+uncertainty_r=(1/tmax)*np.sum((fit_t-np.log(P_reactor))**2)#Standard Deviation
+print(math.sqrt(uncertainty_r))
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
